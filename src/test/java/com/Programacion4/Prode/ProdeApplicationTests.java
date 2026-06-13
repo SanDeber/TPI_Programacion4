bf43697583c6429f7e12012854d30ddae0273d20
Nico-oli <nicoolivieri13@gmail.com>
2026-06-13 13:39:25 -0300

    Inicio del TPI Programacion 4

---
+++ src/test/java/com/Programacion4/Prode/ProdeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Programacion4.Prode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProdeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
