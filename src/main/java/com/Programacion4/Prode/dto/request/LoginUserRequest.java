0f96dea9cd2d64ed639e0cd862f00f1671e5a3f7
Nico-oli <nicoolivieri13@gmail.com>
2026-06-15 17:48:29 -0300

    Registro de usuario con autenticacion

--- src/main/java/com/Programacion4/Prode/dto/request/LoginUserRequest.java
+++ src/main/java/com/Programacion4/Prode/dto/request/LoginUserRequest.java
@@ -2,19 +2,12 @@ package com.Programacion4.Prode.dto.request;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 
 public record LoginUserRequest(
 
-    @Pattern(
-        regexp = "^[a-zA-ZáéíóúÁÉÍÓÚñÑ0-9 ]+$",
-        message = "Solo se permiten números y letras"
-    )
-    @NotBlank(message="El nombre no debe estar en blanco")
-    String name,
-
     @Email(message="El email debe tener el formato *****@***")
+    @NotBlank(message = "el email es obligatorio")
     String email,
 
     @Size(
--- src/main/java/com/Programacion4/Prode/services/impl/AuthService.java
+++ src/main/java/com/Programacion4/Prode/services/impl/AuthService.java
@@ -3,10 +3,15 @@ package com.Programacion4.Prode.services.impl;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.Programacion4.Prode.dto.request.LoginUserRequest;
+import com.Programacion4.Prode.dto.request.RegisterUserRequest;
 import com.Programacion4.Prode.dto.response.LoginResponse;
+import com.Programacion4.Prode.models.Rol;
+import com.Programacion4.Prode.models.User;
+import com.Programacion4.Prode.repository.IUserRepository;
 import com.Programacion4.Prode.security.JwtTokenProvider;
 import com.Programacion4.Prode.services.interfaces.IAuthService;
 
@@ -19,6 +24,8 @@ public class AuthService implements IAuthService {
     private final AuthenticationManager authManager;
     private final UsuarioDetailsService userDetailsService;
     private final JwtTokenProvider tokenProvider;
+    private final IUserRepository userRepo;
+    private final PasswordEncoder passworEncoder;
 
     @Override
     public LoginResponse login(LoginUserRequest request){
@@ -31,5 +38,26 @@ public class AuthService implements IAuthService {
         String rol =    user.getAuthorities().iterator().next().getAuthority();
 
         return new LoginResponse(token, rol);
+    }
+
+    @Override
+    public LoginResponse register(RegisterUserRequest request) {
+        
+        if(userRepo.findByEmail(request.email()).isPresent()){
+            throw new RuntimeException("El email ya existe");
+        }
+
+        User nuevoUsuario = User.builder()
+        .name(request.name())
+        .email(request.email())
+        .password(passworEncoder.encode(request.password()))
+        .rol(Rol.ROLE_USER)
+        .build();
+
+        userRepo.save(nuevoUsuario);
+
+        String token = tokenProvider.generarToken(nuevoUsuario);
+
+        return new LoginResponse(token, nuevoUsuario.getRol().name());
     }
 }
